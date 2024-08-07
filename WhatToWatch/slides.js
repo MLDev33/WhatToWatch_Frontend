c30c172afb1ab593685f8f7d89a500fd231d709b
ClaudiainParis <claudia.mattana@hotmail.com>
2024-08-07 13:10:01 +0200

    onboarding screens setup

---
+++ WhatToWatch/slides.js
@@ -0,0 +1,15 @@
+export default [
+{
+    id: '1',
+    title: ' Welcome to What To Watch ',
+    description: 'Stay informed about the latest releases of movies or series on your favorite platforms (Netflix, Disney+, OCS, etc.). Plan your next movie night alone or with a group!',
+    image: ''
+},
+{
+    id: '2',
+    title: ' Welcome to What To Watch ',
+    description: 'Not sure what to watch on your next date? Create a list of your favorite movie genres, like films or series, and share everything with the group! A match? Perfect, your movie night is planned!',
+    image: ''
+}
+
+]
